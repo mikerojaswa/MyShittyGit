bd03723a113b40f255e3b10a3a141c2549c6f80a
Michael Rojas <mikerojaswa@gmail.com>
2017-09-22 00:29:15 -0700

    And here we go...

---
+++ MyShittyGit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyShittyGit
+//
+//  Created by Michael Rojas on 9/22/17.
+//  Copyright © 2017 Michael Rojas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
